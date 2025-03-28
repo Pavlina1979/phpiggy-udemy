264e826fc66b11b5095b60ed29306960159af937
Pavlína Malinková <pavlina.malinkova@email.cz>
2025-03-28 09:12:20 +0100

    finished section 15 - user registration

--- src/App/Controllers/AuthController.php
+++ src/App/Controllers/AuthController.php
@@ -5,16 +5,18 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use Framework\TemplateEngine;
-use App\Services\ValidatorService;
+use App\Services\{ValidatorService, UserService};
 
 class AuthController
 {
   private TemplateEngine $view;
   private ValidatorService $validatorService;
-  public function __construct(TemplateEngine $view, ValidatorService $validatorService)
+  private UserService $userService;
+  public function __construct(TemplateEngine $view, ValidatorService $validatorService, UserService $userService)
   {
     $this->view = $view;
     $this->validatorService = $validatorService;
+    $this->userService = $userService;
   }
   public function registerView()
   {
@@ -24,5 +26,30 @@ class AuthController
   public function register()
   {
     $this->validatorService->validateRegiste($_POST);
+
+    $this->userService->isEmailTaken($_POST['email']);
+
+    $this->userService->create($_POST);
+
+    redirectTo('/');
+  }
+
+  public function loginView()
+  {
+    echo $this->view->render('/login.php');
+  }
+
+  public function login()
+  {
+    $this->validatorService->validateLogin($_POST);
+    $this->userService->login($_POST);
+
+    redirectTo('/');
+  }
+
+  public function logout()
+  {
+    $this->userService->logout();
+    redirectTo('/login');
   }
 }
--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -15,7 +15,8 @@ class Router
     $this->routes[] = [
       'path' => $path,
       'method' => strtoupper($method),
-      'controller' => $controller
+      'controller' => $controller,
+      'middlewares' => []
     ];
   }
 
@@ -40,7 +41,9 @@ class Router
       $contollerInstance = $container ? $container->resolve($class) : new $class;
       $action = fn() => $contollerInstance->{$function}();
 
-      foreach ($this->middlewares as $middleware) {
+      $allMiddleware = [...$route['middlewares'], ...$this->middlewares];
+
+      foreach ($allMiddleware as $middleware) {
         $middlewareInstance = $container ? $container->resolve($middleware) : new $middleware;
         $action = fn() => $middlewareInstance->process($action);
       }
@@ -54,5 +57,11 @@ class Router
   public function addMiddleware(string $middleware)
   {
     $this->middlewares[] = $middleware;
+  }
+
+  public function addRouteMiddleware(string $middleware)
+  {
+    $lastRouteKey = array_key_last($this->routes);
+    $this->routes[$lastRouteKey]['middlewares'][] = $middleware;
   }
 }
